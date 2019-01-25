1684c63369dfd844f244c44f0e67e93c2133e0bb
Noah Herrin <noahherrin@wlan010-111-069-244.wlan.wwu.edu>
2019-01-25 13:27:50 -0800

    implemented keyboard input interacting with word array:

--- js/io.js
+++ js/io.js
@@ -5,10 +5,9 @@ window.addEventListener('keydown', function(e) {
   var keycode = e.keyCode;
   //input is valid if keycode between 65 and 90
   if (keycode >= 65 && keycode <= 90) {
-    console.log("valid input: " + keycode);
+
     let letter = e.key;
     insertLetter(letter);
-    console.log(word);
   } else {
     switch (keycode) {
       case 8:
@@ -16,18 +15,20 @@ window.addEventListener('keydown', function(e) {
         break;
       case 37:
         moveLeft();
-        ptr -= 1;
         break;
       case 39:
         moveRight();
-        ptr += 1;
         break;
     }
-    console.log(word);
   }
 });
 
 function swap(a, b) {
+  if (a == ptr) {
+    ptr = b;
+  } else if (b == ptr) {
+    ptr = b;
+  }
   var temp = word[a];
   word[a] = word[b];
   word[b] = temp;
@@ -39,30 +40,29 @@ function insertLetter(letter) {
   while ((ptr) < len) {
     swap(len, len - 1);
     len = len - 1;
-    console.log("swapping");
   }
+  ptr++;
 
 }
 
 function moveRight() {
   //make sure right neighbor index is valid
   if (ptr !== word.length - 1) {
+
     swap(ptr, ptr + 1);
-    ptr += 1
   }
 }
 
 function moveLeft() {
-  if (ptr !== 0) {
+  //moving asterisk to a smaller index
+  if (ptr - 1 !== -1) {
     swap(ptr, ptr - 1);
-    ptr -= 1
   }
 }
 
 function deleteLetter() {
-  console.log("trying to delete " + ptr);
+
   if (ptr !== 0) {
-    console.log("confusion");
     word.splice(ptr - 1, 1);
     ptr -= 1;
   }
