89ec3c2a8fed9fa23609abd6e697f7f54e3fb80a
Noah Herrin <noahherrin@Noahs-MacBook-Pro.local>
2019-02-02 15:33:50 -0800

    Complete but broken code

--- public/js/io.js
+++ public/js/io.js
@@ -3,15 +3,7 @@ const vowels = ['a', 'e', 'i', 'o', 'u'];
 var ptr = 0;
 display = document.querySelector("#word");
 window.addEventListener('keydown', function(e) {
-  console.log("it worked");
-  var term = e.keyCode;
-  var url = 'http://localhost:3000/search?term=' + term;
-  fetch(url).then(function(res) {
-    // Res will be a Response object.
-    // Use res.text() or res.json() to get the information that the Node program sent.
-    var a = JSON.parse(res);
-    console.log(a);
-  });
+
   var keycode = e.keyCode;
   //input is valid if keycode between 65 and 90
   if (keycode >= 65 && keycode <= 90) {
@@ -19,33 +11,42 @@ window.addEventListener('keydown', function(e) {
     let letter = e.key;
     if (!isVowel(letter)) {
       insertLetter(letter);
-      display.textContent = toString();
-    } else {
-      console.log("cockblocked")
-    }
+      display.textContent = displayWord();
+    } else {}
 
   } else {
     switch (keycode) {
       case 8:
         deleteLetter();
-        display.textContent = toString();
+        display.textContent = displayWord();
         break;
       case 37:
         moveLeft();
-        display.textContent = toString();
+        display.textContent = displayWord();
         break;
       case 39:
         moveRight();
-        display.textContent = toString();
+        display.textContent = displayWord();
         break;
       case 13:
-        console.log("Submit: " + toString());
+        console.log("Submit: " + displayWord());
+        validateWord(toString());
         break
     }
   }
 });
-
+//Returns the value of the word excluding the asterisk
 function toString() {
+  let result = "";
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] !== '*') {
+      result += word[i];
+    }
+  }
+  return result;
+}
+//Returns the value of the word including the asterisk
+function displayWord() {
   let result = "";
   for (let i = 0; i < word.length; i++) {
     //if (word[i] !== '*') {
@@ -54,7 +55,7 @@ function toString() {
   }
   return result;
 }
-
+//Swaps to elements in the word, and updates necessary pointers
 function swap(a, b) {
   if (a == ptr) {
     ptr = b;
@@ -65,7 +66,7 @@ function swap(a, b) {
   word[a] = word[b];
   word[b] = temp;
 }
-
+//Inserts new letter into word
 function insertLetter(letter) {
   word.push(letter);
   let len = word.length - 1;
@@ -76,7 +77,7 @@ function insertLetter(letter) {
   ptr++;
 
 }
-
+//If new index would not exceed the length of the array, asterisk is moved one space right
 function moveRight() {
   //make sure right neighbor index is valid
   if (ptr !== word.length - 1) {
@@ -84,7 +85,7 @@ function moveRight() {
     swap(ptr, ptr + 1);
   }
 }
-
+//If new index would not exceed the length of the array, asterisk is moved one space left
 function moveLeft() {
   //moving asterisk to a smaller index
   if (ptr - 1 !== -1) {
@@ -117,5 +118,21 @@ function isVowel(letter) {
   }
   return result;
 }
-generateVowels(3);
-display.textContent = toString();
+
+function validateWord(word) { // The id of your input was "begriff", not "name"
+  var url = 'http://localhost:4000/vowel?term=' + word;
+  // Now send a request to your Node program
+  fetch(url).then(function(res) {
+    // Res will be a Response object.
+    // Use res.text() or res.json() to get the information that the Node program sent.
+    var value = res.text().then(function(data) {
+      var result = JSON.parse(data);
+      console.log(data);
+
+
+    });
+
+  });
+}
+generateVowels(2);
+display.textContent = displayWord();
