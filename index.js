ea667a7bf4bdbc48a9fb0b7c071685ada7494610
Noah Herrin <noahherrin@wlan010-111-069-184.wlan.wwu.edu>
2019-02-05 13:32:28 -0800

    Untested transfer of json between server and client

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const dictionary = require("oxford-dictionary-api");
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-var path = require('path');
+const path = require('path');
 
-var app_id = "470defab";
-var app_key = "00f29f6244f11fa9093c86e53cc2de3f";
-var dict = new dictionary(app_id, app_key);
+const app = express();
 app.use("/public", express.static(__dirname + "/public"));
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -15,29 +12,12 @@ app.use(bodyParser.urlencoded({
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
   console.log("New Connection");
-
-  //res.send("did you say " + term + "?");
 });
-app.get("/vowel", function(req, res) {
-
-  var temp = req.url;
-  var term = req.url.slice(12, temp.length);
-  lookup(term, res);
+app.get('/data', function(req, res) {
+  var object = new Object();
+  object.response = false;
+  res.send(JSON.stringify(object));
 });
-
-function lookup(word, res) {
-  var lookup = dict.find(word);
-  lookup.then((res) => {
-      // stringify JSON object to see full structure in console log
-      console.log(word + "is a real word");
-      res.send({ response: "correct" });
-    },
-    (error) => {
-      console.log(word + "not a real word");
-      res.send({ response: "incorrect" });
-    });
-}
-
 app.listen(4000, function() {
   console.log("Starting Server");
 });
--- public/js/io.js
+++ public/js/io.js
@@ -1,138 +1,14 @@
-var word = ['*'];
-const vowels = ['a', 'e', 'i', 'o', 'u'];
-var ptr = 0;
-display = document.querySelector("#word");
-window.addEventListener('keydown', function(e) {
+const url = "http://localhost:4000/data";
 
-  var keycode = e.keyCode;
-  //input is valid if keycode between 65 and 90
-  if (keycode >= 65 && keycode <= 90) {
+// Now send a request to your Node program
+fetch(url).then(function(res) {
+  // Res will be a Response object.
+  // Use res.text() or res.json() to get the information that the Node program sent.
+  var value = res.text().then(function(data) {
+    var result = JSON.parse(data);
+    console.log(result.response === true);
 
-    let letter = e.key;
-    if (!isVowel(letter)) {
-      insertLetter(letter);
-      display.textContent = displayWord();
-    } else {}
-
-  } else {
-    switch (keycode) {
-      case 8:
-        deleteLetter();
-        display.textContent = displayWord();
-        break;
-      case 37:
-        moveLeft();
-        display.textContent = displayWord();
-        break;
-      case 39:
-        moveRight();
-        display.textContent = displayWord();
-        break;
-      case 13:
-        console.log("Submit: " + displayWord());
-        validateWord(toString());
-        break
-    }
-  }
-});
-//Returns the value of the word excluding the asterisk
-function toString() {
-  let result = "";
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] !== '*') {
-      result += word[i];
-    }
-  }
-  return result;
-}
-//Returns the value of the word including the asterisk
-function displayWord() {
-  let result = "";
-  for (let i = 0; i < word.length; i++) {
-    //if (word[i] !== '*') {
-    result += word[i];
-    //}
-  }
-  return result;
-}
-//Swaps to elements in the word, and updates necessary pointers
-function swap(a, b) {
-  if (a == ptr) {
-    ptr = b;
-  } else if (b == ptr) {
-    ptr = b;
-  }
-  var temp = word[a];
-  word[a] = word[b];
-  word[b] = temp;
-}
-//Inserts new letter into word
-function insertLetter(letter) {
-  word.push(letter);
-  let len = word.length - 1;
-  while ((ptr) < len) {
-    swap(len, len - 1);
-    len = len - 1;
-  }
-  ptr++;
-
-}
-//If new index would not exceed the length of the array, asterisk is moved one space right
-function moveRight() {
-  //make sure right neighbor index is valid
-  if (ptr !== word.length - 1) {
-
-    swap(ptr, ptr + 1);
-  }
-}
-//If new index would not exceed the length of the array, asterisk is moved one space left
-function moveLeft() {
-  //moving asterisk to a smaller index
-  if (ptr - 1 !== -1) {
-    swap(ptr, ptr - 1);
-  }
-}
-
-function deleteLetter() {
-  if (ptr !== 0 && !isVowel(word[ptr - 1])) {
-    word.splice(ptr - 1, 1);
-    ptr -= 1;
-  }
-}
-
-function generateVowels(difficulty) {
-  for (let i = 0; i < difficulty; i++) {
-    let i = Math.floor(Math.random() * (vowels.length - 1));
-    word.push(vowels[i]);
-  }
-
-
-}
-
-function isVowel(letter) {
-  let result = false;
-  for (let i = 0; i < vowels.length; i++) {
-    if (letter === vowels[i]) {
-      result = true;
-    }
-  }
-  return result;
-}
-
-function validateWord(word) { // The id of your input was "begriff", not "name"
-  var url = 'http://localhost:4000/vowel?term=' + word;
-  // Now send a request to your Node program
-  fetch(url).then(function(res) {
-    // Res will be a Response object.
-    // Use res.text() or res.json() to get the information that the Node program sent.
-    var value = res.text().then(function(data) {
-      var result = JSON.parse(data);
-      console.log(data);
-
-
-    });
 
   });
-}
-generateVowels(2);
-display.textContent = displayWord();
+
+});
